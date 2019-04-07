78e3b6299c663ad87a4c31ff6767980d9f22bfaf
BebopVinh <vinhdo07@gmail.com>
2019-04-07 10:37:01 -0700

    creating users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,31 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "my_secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    binding.pry
+    @username = params[:username]
+    @password = params[:password]
+    @email = params[:email]
+    unless @username.empty? || @password.empty? || @email.empty?
+      User.create(params)
+    end
+    #make new user here
+    redirect '/tweets'
+  end
+
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
 end
