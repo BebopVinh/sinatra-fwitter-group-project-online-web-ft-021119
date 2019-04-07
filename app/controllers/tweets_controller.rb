525116b90cef9b472a24ea3730ed23ea44268fbb
bebopvinh <vinhdo07@gmail.com>
2019-04-07 14:18:23 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,7 +7,74 @@ class TweetsController < ApplicationController
       else
          redirect '/login'
       end
-    end
+   end
     
+   post '/tweets/new' do
+      if logged_in?
+         redirect '/tweets/new'
+      else
+         redirect '/login'
+      end
+   end
+
+   get '/tweets/new' do
+      if logged_in?
+         erb :'/tweets/new'
+      else
+         redirect '/login'
+      end
+   end
+
+   post '/tweets' do
+      @user = User.find(session[:user_id])
+      unless params[:tweet][:content].empty?
+         @user.tweets << Tweet.new(params[:tweet])
+         redirect "/users/#{@user.slug}"
+      else
+         redirect '/tweets/new'
+      end
+   end
+
+   get '/tweets/:id' do
+      if logged_in?
+         @tweet = Tweet.find(params[:id])
+         erb :'/tweets/show_tweet'
+      else
+         redirect '/login'
+      end
+   end
+
+   post '/tweets/:id' do
+      redirect "/tweets/#{param[:id]}/edit"
+   end
+
+   get '/tweets/:id/edit' do
+      if logged_in?
+         @tweet = Tweet.find(params[:id])
+         erb :'/tweets/edit_tweet' if @tweet.user.id == session[:user_id]
+      else
+         redirect '/login'
+      end
+   end
    
+   patch '/tweets/:id' do
+      unless params[:tweet][:content].empty?
+         @tweet = Tweet.find(params[:id])
+         @tweet.update(params[:tweet])
+         redirect "/tweets/#{@tweet.id}"
+      else
+         redirect "/tweets/#{params[:id]}/edit"
+      end
+   end
+
+   delete '/tweets/:id' do
+      if logged_in?
+         @tweet = Tweet.find(params[:id])
+         if @tweet.user.id == session[:user_id]
+            @tweet.destroy
+         end
+      else
+         redirect '/login'
+      end
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
    has_many :tweets
 
    def slug
-      self.username.downcase.gsub(/[^0-9a-z\- ]/, "")
+      self.username.downcase.gsub(/[^0-9a-z\- ]/, "").gsub(" ", "-")
    end
 
    def self.find_by_slug(name)
-      user = User.find
+      user = User.find {|user| user.slug == name}
    end
 end
